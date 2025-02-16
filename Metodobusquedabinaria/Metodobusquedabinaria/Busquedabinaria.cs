371faf1accc3349893455fa2588a13b1009653b8
J0sh2210 <jramireza31@miumg.edu.gt>
2025-02-15 23:04:41 -0600

    Creación de búsqueda binaria

---
+++ Metodobusquedabinaria/Metodobusquedabinaria/Busquedabinaria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodobusquedabinaria
+{
+    public class Busquedabinaria
+    {
+        public static int binarysearch(int[] num, int x)
+        {
+            int low = 0, high = num.Length;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (num[mid] == x)
+                {
+                    return mid;
+                } else if (num[mid] < x) {
+                    low = mid + 1;
+                }else
+                {
+                    high = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Metodobusquedabinaria/Metodobusquedabinaria/Program.cs
@@ -0,0 +1,31 @@
+﻿using Metodobusquedabinaria;
+using System;
+namespace BB
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, };
+            Console.WriteLine("El arreglo es: ");
+            foreach (int i in num)
+            {
+                Console.Write(i + " , ");
+            }
+            int x = 4;
+
+            int indice = Busquedabinaria.binarysearch(num, x);
+
+            if (indice == -1)
+            {
+                Console.WriteLine("La búsqueda no existe");
+            } else
+            {
+                Console.WriteLine(" el elemento {0} existe en la posición {1}", x, indice);
+            }
+            
+
+
+        }
+    }
+}
