cf835d5a476c382f2a579010069ac50a7851efa7
J0sh2210 <jramireza31@miumg.edu.gt>
2025-02-15 23:14:16 -0600

    Creación busqueda Lineal

---
+++ BusquedaLineal/BusquedaLineal/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BusLi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+            Console.WriteLine("El arreglo es: ");
+            foreach (int i in num)
+            {
+                Console.WriteLine(i + " , ");
+            }
+
+            int x = 8;
+
+        }
+    }
+}
